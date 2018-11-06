001e795dabb5bc07a60cf3138ae99e4a20d81a2c
erikba <erik.baltazar@hotmail.com>
2018-11-06 17:58:07 -0600

    primer commit solo varibles

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+
+public static void main(String[] args) {
+   
+    Movie movie=new Movie();
+    int exit=0;
+    
+    do {
+        System.out.println("BIENVENIDOS A AMAZON VIEWER");
+        System.out.println("");
+        System.out.println("SELECCIONA EL NUMERO DE LA OPCION QUE DECEAAS");
+        System.out.println("1. MOVIES");
+        System.out.println("2. SERIES");
+        System.out.println("3. BOOKS");
+        System.out.println("4. MAGAZINES");
+        System.out.println("0. EXIT");
+        
+        //LEER LA RESPUESTA DEL USUARIO
+        
+        
+        int response=0;
+        switch (response=0){
+            case 0:
+            //salir
+            exit=0;
+            break;
+            case 1:
+                
+            break;
+            case 2:
+                
+            break;
+            case 3:
+                
+            break;
+            case 4:
+                
+            break;
+            case 5:
+                
+            break;
+            case 6:
+                
+            break;
+   
+            default:
+                System.out.println("");
+                System.out.println("....¡¡ seleccione una opcion correcta !!....");
+                System.out.println("");
+                break;
+        }
+        
+    } while (exit!=0);
+    
+}
+    
+}
